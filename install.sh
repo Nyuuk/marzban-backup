7e2a441fd654eb02cebe36f58db4efe067e5bf4e
NyuukLaptop <dnanfab@gmail.com>
2023-08-02 09:15:55 +0700

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ else
 fi
 
 # Ganti dengan jalur lengkap menuju skrip backup_to_github.sh
-SCRIPT_PATH="$(pwd)/start.sh"
+SCRIPT_PATH="$(pwd)/boot_script.sh"
 
 echo "cd $(pwd); ./start.sh > 2>&1" > $(pwd)/boot_script.sh
 chmod +x $(pwd)/boot_script.sh
 
 # Tambahkan entri ke crontab jika belum ada
 if ! crontab -l | grep -q "@reboot $SCRIPT_PATH"; then
-  (crontab -l 2>/dev/null; echo "@reboot $(pwd)/boot_script.sh 2>&1") | crontab -
+  (crontab -l 2>/dev/null; echo "@reboot $SCRIPT_PATH") | crontab -
   echo "Entri berhasil ditambahkan ke crontab."
 else
   echo "Entri sudah ada di crontab. Tidak ada yang perlu ditambahkan."
